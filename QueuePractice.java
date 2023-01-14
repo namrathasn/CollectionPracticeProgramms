1c339efb64aa265c527a432be29888ab59bcb269
namrathasn <namrathasn199819@gmail.com>
2023-01-14 22:39:51 +0530

    new

---
+++ QueuePractice.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class QueuePractice {
+    public static void main(String args[]){
+        PriorityQueue<String> queue=new PriorityQueue<String>();
+        // creating priority queue 
+        queue.add("Amit");
+        // adding elements 
+        queue.add("Rachit");
+        queue.add("Rahul");
+        System.out.println("head:"+queue.element());
+        System.out.println("head:"+queue.peek());
+        System.out.println("iterating the queue elements:");
+        Iterator itr=queue.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        queue.remove();
+        queue.poll();
+        System.out.println("after removing two elements:");
+        Iterator<String> itr2=queue.iterator();
+        while(itr2.hasNext()){
+            System.out.println(itr2.next());
+        }
+    }
+}
